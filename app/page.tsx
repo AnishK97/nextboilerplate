860bd804cbe74a1c2fbab1d23676f64231c723d0
AnishK97 <anishkulandaisamy@outlook.com>
2024-04-03 17:31:27 +0100

    Boiler plate with supabase logins

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <Button>Saas Boilerplate</Button>
+    </div>
+  )
+}
+
